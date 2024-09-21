41ba6d79295232e594541b9391574a21132e262b
Sleep1ng-Giant <kakhanzada@captechu.edu>
2024-09-21 01:47:07 -0400

    making the my_players.json file human readable

--- get-players.py
+++ get-players.py
@@ -12,5 +12,15 @@ def save_players_to_file(players, filename='players.json'):
         
         
 players = get_player_data()
-save_players_to_file(players, 'my_players.json')
+#save_players_to_file(players, 'my_players.json')
 
+def save_prettified_json(player_data, filename='players_pretty.json'):
+    with open(filename, 'w') as file:
+        # Save the JSON with an indent of 4 for pretty formatting
+        json.dump(player_data, file, indent=4)
+
+    print(f"Player data saved to {filename} in prettified format.")
+    
+player_data = get_player_data()  
+save_prettified_json(player_data, 'players_pretty.json')
+    
